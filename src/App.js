292a264c9b15a6cf9f77fce4fd13aad64d24893d
jpcljpcljpcl <joaopaulolopes_@outlook.com>
2023-12-11 16:03:18 +0000

    Updated Resumee

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import './App.css';
-import PdfPage from './PdfPage';
-import cv from './CVJPCL.jpg';
+import "./App.css";
+import PdfPage from "./PdfPage";
+import cv from "./CVJPCL.jpg";
 
 function App() {
   return (
     <div className="App">
       {/* <PdfPage /> */}
       <img style={{ width: "100%", height: "100%" }} src={cv} alt="pdf" />
+      <a target="_blank" href="https://www.zaask.pt/user/joaopaulolopes">
+        <img
+          src="https://www.zaask.pt/widget?user=812882&widget=pro-findme"
+          alt=""
+        />
+      </a>
     </div>
   );
 }
